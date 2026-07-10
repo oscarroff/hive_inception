255bacbb7c62c99cb5fc991793f867e4bc6c9043
Thomas Black-Roff <thblack-@c1r5p16.hive.fi>
2026-07-10 18:09:32 +0300

    fix: password added to DB init.sh shutdown and cleanup of .sh file

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,6 +4,9 @@
 # Exit immediately if anything fails
 set -e
 
+# Set SOCKET variable which we will reuse several times
+SOCKET=/run/mysqld/mysqld.sock
+
 # Create the run directory and let the mysql user own the directory and data
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld /var/lib/mysql
@@ -15,29 +18,25 @@ fi
 
 # Start MariaDB in the background (no TCP) so we can run commands for
 # initialization
-mysqld --user=mysql --skip-networking --socket=/run/mysqld/mysqld.sock \
-	& pid="$!"
+mysqld --user=mysql --skip-networking --socket="SOCKET" & pid="$!"
 
 # Wait until server is ready
-until mariadb-admin --socket=/run/mysqld/mysqld.sock ping >/dev/null 2>&1; do
+until mariadb-admin --socket="SOCKET" ping >/dev/null 2>&1; do
     sleep 1
 done
 
 # Setup user and password from .env
-DB_CMD=(mariadb --socket=/run/mysqld/mysqld.sock -e)
-"${DB_CMD[@]}" "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+DB_CMD=(mariadb --socket="SOCKET" -e)
+"${DB_CMD[@]}" "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 "${DB_CMD[@]}" "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' \
-	IDENTIFIED BY '${MYSQL_PASSWORD}';"
-"${DB_CMD[@]}" "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* \
-	TO '${MYSQL_USER}'@'%';"
-"${DB_CMD[@]}" "SET PASSWORD FOR 'root'@'localhost' \
-	= PASSWORD('${MYSQL_ROOT_PASSWORD}');" || true
-"${DB_CMD[@]}" "ALTER USER 'root'@'localhost'\
-	IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+IDENTIFIED BY '${MYSQL_PASSWORD}';"
+"${DB_CMD[@]}" "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';"
+"${DB_CMD[@]}" "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 "${DB_CMD[@]}" "FLUSH PRIVILEGES;"
 
 # Stop background MariaDB after changes have been made
-mysqladmin --socket=/run/mysqld/mysqld.sock shutdown || kill "$pid"
+mariadb-admin --socket="SOCKET" -uroot \
+-p"${MYSQL_ROOT_PASSWORD}" shutdown || kill "$pid"
 wait "$pid" 2>/dev/null || true
 
 # Start MariaDB in the foreground as PID1

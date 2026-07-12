cd8369ce6831a7fafe5e20f496be0eea00165555
Thomas Black-Roff <thblack-@c1r6p12.hive.fi>
2026-07-12 21:24:41 +0300

    bug: finding error with basic setup

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,45 +1,12 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-# Download WordPress
-wget -q https://wordpress.org/latest.tar.gz -P /tmp
+mkdir -p /var/www/html /run/php /tmp
+
+# Download WordPress tarball with curl (fails hard if download fails)
+curl -fsSL https://wordpress.org/latest.tar.gz -o /tmp/latest.tar.gz
 tar -xzf /tmp/latest.tar.gz -C /tmp
-mkdir -p /var/www/html
-mv /tmp/wordpress/* /var/www/html/
+cp -a /tmp/wordpress/. /var/www/html/
 
-# Download WP-CLI
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-
-# Wait for MariaDB to be ready
-sleep 5
-
-# Create wp-config.php
-wp config create \
---dbname=wordpress \
---dbuser=wpuser \
---dbpass=wppassword \
---dbhost=mariadb \
---path=/var/www/html \
---allow-root
-
-# Install WordPress
-wp core install \
---url=localhost \
---title="Inception" \
---admin_user=myadmin \
---admin_password=adminpass \
---admin_email=admin@example.com \
---path=/var/www/html \
---allow-root
-
-# Create editor user
-wp user create editor editor@example.com \
---role=editor \
---user_pass=editorpass \
---path=/var/www/html \
---allow-root
-
-# Start PHP-FPM
-mkdir -p /run/php
-exec php-fpm7.4 -F
+# Start php-fpm in foreground (Debian 12)
+exec php-fpm8.2 -F
